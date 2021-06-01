07e2f5ccd2aee24c7df87cc82a33336d401d3e7a
Alex-A-B <alexjamesbelton@gmail.com>
2021-06-01 10:02:11 +0100

    each function working in library

--- index.js
+++ index.js
@@ -4,15 +4,25 @@ const fi = (function() {
       return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
     },
 
-    each: function() {
+    each: function(collection, callback) {
+      let newCollection = collection;
+        if (collection instanceof Array) {
+            newCollection = collection.slice()
+        } else {
+            newCollection = Object.values(collection)
+        }
+
+        for (let cb = 0; cb < newCollection.length; cb++)
+            callback(newCollection[cb])
+
+      return collection
+    },
+
+    map: function(collection, callback) {
 
     },
 
-    map: function() {
-
-    },
-
-    reduce: function() {
+    reduce: function(collection, callback, acc) {
 
     },
 
